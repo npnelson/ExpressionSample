a88c53826335b667ba27301a7a2e5473d99d7dae
npnelson <nnelson1@insight.rr.com>
2016-08-03 16:07:42 -0400

    initial commit

---
+++ ExpressionTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExpressionTest")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("85a295c4-d7a5-42a7-b413-049dbf9eee1e")]
---
+++ FullFrameworkTest/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FullFrameworkTest
+{
+    public class Tests
+    {
+        [Fact]
+        public void TestTest()
+        {
+            Assert.True(false);
+        }
+    }
+}
